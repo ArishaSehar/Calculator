f6ee942430c17e9d97684896498ea267b51871f5
arishasehar <arshsehar66@gmail.com>
2024-08-11 21:22:51 +0500

    calculator

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
-var value = document.getElementById("result")
+// var value = document.getElementById("result")
+// function getNumber(element) {
+//     let elementValue = element.innerText
+//     value.value += elementValue
+// }
+// function clearValue(){
+//     value.value = " "
+// }
+// function getResult() {
+//     value.value = eval(value.value)
+// }
+
+// function del() {
+//     let data = document.getElementById("result");
+//     data.value = data.value.substr(0, data.value.length - 1);
+// }
+
+var value = document.getElementById("result");
+    
 function getNumber(element) {
-    let elementValue = element.innerText
-    value.value += elementValue
+    let elementValue = element.innerText;
+    value.value += elementValue;
 }
-function clearValue(){
-    value.value = " "
+
+function clearValue() {
+    value.value = "";
 }
+
 function getResult() {
-    value.value = eval(value.value)
+    try {
+        value.value = eval(value.value);
+    } catch (e) {
+        value.value = "Error";
+    }
 }
 
 function del() {
-    let data = document.getElementById("result");
-    data.value = data.value.substr(0, data.value.length - 1);
+    value.value = value.value.substr(0, value.value.length - 1);
 }
